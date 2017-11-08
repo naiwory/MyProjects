f795ada86f1f0ba09640cb42476a542f6a7199cd
naiwory <naiwory.rainbow@gmail.com>
2017-11-08 11:58:45 +0200

    Started module 7

---
+++ src/main/java/module7/Client.java
@@ -0,0 +1,44 @@
+package module7;
+
+public class Client {
+
+    private String name;
+    private Fruit.FruitType fruit;
+    private int count;
+
+    public Client(){
+        name = "unknown";
+        fruit = Fruit.FruitType.apple;
+        count = 1;
+    }
+
+    public Client(String name, Fruit.FruitType fruit, int count) {
+        this.name = name;
+        this.fruit = fruit;
+        this.count = count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Fruit.FruitType getFruit() {
+        return fruit;
+    }
+
+    public void setFruit(Fruit.FruitType fruit) {
+        this.fruit = fruit;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
---
+++ src/main/java/module7/Fruit.java
@@ -0,0 +1,59 @@
+package module7;
+
+public class Fruit {
+
+    private FruitType fruit;
+    private int shelfLife;
+    private String dateOfIncome;
+    private double price;
+
+    public Fruit(){
+        fruit = FruitType.apple;
+        shelfLife = 10;
+        dateOfIncome = "unknown";
+        price = 15;
+    }
+
+    public Fruit(FruitType fruit, int shelfLife, String dateOfIncome, double price) {
+        this.fruit = fruit;
+        this.shelfLife = shelfLife;
+        this.dateOfIncome = dateOfIncome;
+        this.price = price;
+    }
+
+    public enum FruitType {
+        apple, strawberry, lemon, peach, orange, banana, watermelon, blueberry, cloudberry
+    }
+
+    public FruitType getFruit() {
+        return fruit;
+    }
+
+    public void setFruit(FruitType fruit) {
+        this.fruit = fruit;
+    }
+
+    public int getShelfLife() {
+        return shelfLife;
+    }
+
+    public void setShelfLife(int shelfLife) {
+        this.shelfLife = shelfLife;
+    }
+
+    public String getDateOfIncome() {
+        return dateOfIncome;
+    }
+
+    public void setDateOfIncome(String dateOfIncome) {
+        this.dateOfIncome = dateOfIncome;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
